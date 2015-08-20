b0cfe35d588733287228b7656a0fda1e1e79356b
Mohit Kumar <mhtkmr54@gmail.com>
2015-08-20 15:24:40 +0530

    added dijkstra

--- sherlock_and_watson.py
+++ sherlock_and_watson.py
@@ -20,27 +20,16 @@ print query
 
        
 def rotate(array):
-  print "////////////////input array/////////////////////////"
-  print array
   mirror = [None]*N
-  print mirror
   for i in xrange(N-1):
-    print i
     mirror[i+1] = array[i]
     mirror[0] = array[N-1]
-    print mirror
     
-  print mirror
-  print  "//////rotaaaaaaaaaaated////////////////////"
   array  = mirror
-  print "///////////array///////////////////////"
-  print array
   return array 
  
 for i in xrange(K):
-  print i
   array = rotate(array)
-  print array 
 
 for i in xrange(len(query)):
   print array[query[i]]  
