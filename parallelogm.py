ba98a136a6bbbb49665c645071855909ee6009b3
Mohit Kumar <mhtkmr54@gmail.com>
2015-09-19 17:44:13 +0530

    cases

--- parallelogm.py
+++ parallelogm.py
@@ -18,18 +18,23 @@ def check_para(coords):
    bc = [p[2][0]-p[1][0],p[2][1]-p[1][1]]
    cb =  map(lambda x: x * -1, bc)
 
+  # 3 cases if parallelogram can be picked up out of the plane and turned over
 
    if ac == bd and ab == cd:
         print "parallelogram exists as given"
-        print p[0],p[2],p[3],p[1]
+        print "seen clockwise from first coordinate ",p[0],p[1],p[3],p[2]
+        print "seen anti-clockwise from first coordinate ",p[0],p[2],p[3],p[1]
    
    elif  ac == db and ad == cb:
         print "parallelogram exists as given"
-        print p[0],p[2],p[1],p[3]
+        print "seen clockwise from first coordinate ",p[0],p[2],p[1],p[3]
+        print "seen anti-clockwise from first coordinate ",p[0],p[3],p[1],p[2]
+
    
    elif ad == bc and ab == dc:
         print "parallelogram exists as given"
-        print p[0],p[3],p[2],p[1]
+        print "seen clockwise from first coordinate ",p[0],p[3],p[2],p[1]
+        print "seen anti-clockwise from first coordinate ",p[0],p[1],p[2],p[3]
 
         
    else:
