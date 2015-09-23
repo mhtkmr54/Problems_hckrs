17350b8798fdc43ba8263943e7b25f4d49f11210
Mohit Kumar <mhtkmr54@gmail.com>
2015-09-23 13:04:32 +0530

    heap

---
+++ heapq.py
@@ -0,0 +1,4 @@
+import heapq
+listForTree = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]    
+heapq.heapify(listForTree)             # for a min heap
+heapq._heapify_max(listForTree)        # for a maxheap!!
