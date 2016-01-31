21a6317592d919a8d7ff75abc33fd45f443e72b7
Mohit Kumar <mhtkmr54@gmail.com>
2016-01-31 21:26:32 +0530

    facedetect

---
+++ face_detect.py
@@ -0,0 +1,47 @@
+import cv2
+import sys
+
+# Get user supplied values
+
+cam = cv2.VideoCapture()
+
+def getframe(name):  
+  cam.open(0)
+  img=cam.read()
+  cv2.imwrite(str(name)+".jpg",img[1])
+  cam.release() 
+
+
+getframe("snap")
+
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+# Read the image
+image = cv2.imread("snap.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+)
+
+print "Found {0} faces!".format(len(faces))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = image[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+cv2.imshow("Faces found and eyes probably", image)
+cv2.waitKey(0)
