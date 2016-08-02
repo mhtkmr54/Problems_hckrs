90d3228098bdae1ec92a7bb11140d3aff7d397d5
mhtkmr54 <mhtkmr54@gmail.com>
2016-08-02 09:10:17 +0530

    test cses cleared

--- board_cuts_bttr.py
+++ board_cuts_bttr.py
@@ -8,25 +8,38 @@ for i in xrange(T):
 Y.sort(reverse=True)
 X.sort(reverse=True)
 
+#print Y
+#print X
+
 y_cut = x_cut = 1     #To count the number of segments
 cost = i = j = 0
 
 while i < X.__len__() and j < Y.__len__():
+    #print i
+    #print "X arra", X
+    #print X[i],"or", Y[j]
     if X[i] >= Y[j]:
+        #print "selected X" ,X[i]
         x_cut = x_cut + 1
-        cost = cost + (X[i]*y_cut)
+        cost = cost + X[i]*y_cut
+        #print "cost", cost
         i = i+1
     else:
+        #print "selected Y" ,Y[j]
         y_cut = y_cut + 1
-        cost = cost + (Y[j]*x_cut)
+        cost = cost + Y[j]*x_cut
         j = j+1
+#print "i,j"
+#print i,j
 
 while i < X.__len__():
-    cost = cost + (X[i]*y_cut)
+    #print "selected Y len Exhausted" ,X[i]
+    cost = cost + X[i]*y_cut
     i = i+1
 
 while j < Y.__len__():
-    cost = cost + (Y[j]*x_cut)
+    #print "selected X len Exhausted" ,Y[j]
+    cost = cost + Y[j]*x_cut
     j = j+1
 
 print cost
