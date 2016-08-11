094294b0fc33c3f87d131d5907c07f1d8e4de1d2
mhtkmr54 <mhtkmr54@gmail.com>
2016-08-11 21:46:28 +0530

    test case pass

--- matching_sets.py
+++ matching_sets.py
@@ -2,38 +2,59 @@
 
 length = raw_input()
 X = map(int,raw_input().strip().split(' '))
-Y = map(int,raw_input().strip().split(  ' '))
-   
+Y = map(int,raw_input().strip().split( ' '))
+
 op = 0
 
-inter = list(set(X).intersection(Y))
 
-while inter:
-    for el in inter:
-        X.pop(X.index(el))
-        Y.pop(Y.index(el))
-    inter = list(set(X).intersection(Y))
-          
 def find_operations(X,Y,op):
+    if X == Y:
+        return op
+    inter = list(set(X).intersection(Y))
+    #print X, "XXXXXXXXXXXXX"
+    #print Y, "YYYYYYYYYYY"
+    #print inter, "inter"
     while inter:
         for el in inter:
             X.pop(X.index(el))
             Y.pop(Y.index(el))
         inter = list(set(X).intersection(Y))
+    #print X, "After rem inter X"
+    #print Y, "After rem exter Y"
     if max(X) < min(Y) or min(X) > max(Y):
+        print -1
         return -1
     elif max(X) > min(Y) and max(X) < max(Y) and min(X) < min(Y):
+        print -1
         return -1
     elif min(X) > min(Y) and min(X) < max(Y) and max(X) > max(Y):
+        print -1
         return -1
     elif max(X) > max(Y) and min(X) < min(Y):
-        a(a.index(max(a))) -= 1
-        a(a.index(min(a))) += 1
+        X[X.index(max(X))] -= 1
+        X[X.index(min(X))] += 1
         op += 1
-        find_operations(X,Y,op)
-       
-print find_operations(X,Y,op)
-    
-    
-    
-    
+        if X == Y:
+            print op
+            return op
+        else:
+            find_operations(X,Y,op)
+    elif max(X) < max(Y) and min(X) > min(Y):
+        #print "X is short range"
+        X[X.index(max(X))] += 1
+        X[X.index(min(X))] -= 1
+        op += 1
+        #print X, "After Short to long op X"
+        #print Y, "as it is Y"
+        if X == Y:
+            print op
+            return op
+        else:
+            find_operations(X,Y,op)
+
+
+find_operations(X,Y,op)
+
+
+
+
